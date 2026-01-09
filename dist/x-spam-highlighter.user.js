696b95ef79056bdc87b4f41698cb2fec1ff93b5a
shapoco <40152459+shapoco@users.noreply.github.com>
2026-01-09 22:45:02 +0900

    fix rule

--- dist/x-spam-highlighter.user.js
+++ dist/x-spam-highlighter.user.js
@@ -4,7 +4,7 @@
 // @updateURL   https://github.com/shapoco/x-spam-highlighter/raw/refs/heads/main/dist/x-spam-highlighter.user.js
 // @downloadURL https://github.com/shapoco/x-spam-highlighter/raw/refs/heads/main/dist/x-spam-highlighter.user.js
 // @match       https://x.com/*
-// @version     1.4.496
+// @version     1.4.498
 // @author      Shapoco
 // @description フォロワー欄でスパムっぽいアカウントを強調表示します
 // @run-at      document-start
@@ -102,9 +102,14 @@
     { regexes: [/人见知り?/g], add: 20 },
     { regexes: [/会话|话(す|せ|多め)/g], add: 20 },
     { regexes: [/闻く/g], add: 20 },
-    { regexes: [/现实|実际/g], add: 20 },
+    { regexes: [/现实/g], add: 20 }, // 現実
+    { regexes: [/実际/g], add: 20 }, // 実際
     { regexes: [/一绪/g], add: 20 },
     { regexes: [/终わり/g], add: 20 },
+    { regexes: [/逛街/g], add: 20 }, // 街歩き
+    { regexes: [/聊天/g], add: 20 }, // チャット
+    { regexes: [/重视/g], add: 20 },
+    { regexes: [/关系/g], add: 20 }, // 関係
     { regexes: [REGEX_SEXUAL_DESIRE], add: 20 },
     { regexes: [REGEX_MEDIA, /(オナ|えっ?ちな?|丸見え|大人|オトナ)/g], add: 20 },
     { regexes: [REGEX_MEDIA, REGEX_CASTING], add: 10 },
